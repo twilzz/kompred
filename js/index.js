c24bb5877256c84a53ef9671963c43881f64d004
Alexey aka Twils <twils@ya.ru>
2020-09-16 23:36:01 +0300

    смена логики

--- js/index.js
+++ js/index.js
@@ -1,49 +1,46 @@
 const chkBoxes = document.querySelectorAll('input'),
-      resultDiv = document.querySelector('#result'),
-      formButton = document.querySelector('button');
+      resultDiv = document.querySelector('#result');
+      
 
-formButton.addEventListener('click', (e)=> {
-    e.preventDefault();
-    chkBoxes.forEach((item, id) => {
-        if (item.checked) {
-            const newEntry = document.createElement('div');
-            newEntry.innerHTML = `<li>${item.nextSibling.nodeValue}</li>`;
-            newEntry.classList.add(`line`);
-            resultDiv.appendChild(newEntry);
-        }
-    });
-showPositions();
-});
+// formButton.addEventListener('click', (e)=> {
+//     e.preventDefault();
+//     chkBoxes.forEach((item, id) => {
+//         if (item.checked) {
+//             const newEntry = document.createElement('div');
+//             newEntry.innerHTML = `<li>${item.nextSibling.nodeValue}</li>`;
+//             newEntry.classList.add(`line`);
+//             resultDiv.appendChild(newEntry);
+//         }
+//     });
+// showPositions();
+// });
 
-function showPositions() {
-const posLine = document.querySelectorAll('li');
-posLine.forEach(item => {
-    item.addEventListener('click', async ()=> {
-    console.dir(item);
-    const fetchResult = await showDoors();
-    
-    for (let key in fetchResult) {
-        console.log(key);
-        const newEntry = document.createElement('div');
-        newEntry.classList.add('entry');
-        newEntry.innerHTML = `${key[0]}`;
-        item.parentElement.after(newEntry);
-        }
+console.log(chkBoxes);
+
+chkBoxes.forEach((item, id) => {
+    item.addEventListener('click', (e)=> {
+    console.log(`Генерируем ${item.nextSibling.data}`);
+    showDB(id).then(data => generatePosition(data));
     });
 });
-}
+        
 
-async function showDb (){
-    const fetchResult = await showDoors();
-    console.log(fetchResult[0]["Потолки"]);
-    fetchResult[0]["Потолки"].forEach(key => console.log(key))
-}
-
-showDb();
-async function  showDoors () {
+async function  showDB (id) {
     const q = await fetch('db.json');
     const data = await q.json();
-    return data;
+    return data[id];
  }
 
-
+function generatePosition(data) {
+    let string = '';
+    if (data.type.length > 1) {
+        for (let i = 0; i<data.type.length; i++ ) {
+            string = `${string} ${data.type[i]} по цене ${data.price[i]}р/м2, коэффициент ${data.kfc[i]}<br>`;
+        }
+    }
+    const newEntry = document.createElement('div');
+    newEntry.innerHTML = `<li>${data.name}</li> <br>
+    содержит типы:<br> ${string}`;
+    newEntry.classList.add(`line`);
+    resultDiv.appendChild(newEntry);
+}
